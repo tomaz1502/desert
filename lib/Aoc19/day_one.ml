d7ea121625ac329cdb5d0ed4e2048a6f441dbcd8
tomaz1502 <tomgm1502@gmail.com>
2023-10-21 18:18:41 -0300

    better day one

--- lib/Aoc19/day_one.ml
+++ lib/Aoc19/day_one.ml
@@ -1,18 +1,20 @@
+let rec read_file rc : string list =
+    try
+        let l = input_line rc in
+        let r = read_file rc in
+        l :: r
+    with End_of_file -> []
+
+let rec solve x = 
+    let y = (x / 3) - 2 in
+    if y <= 0 then 0 else y + solve y
+
 let () =
-    let rec read_file rc : string list =
-        try
-            let l = input_line rc in
-            let r = read_file rc in
-            l :: r
-        with End_of_file -> []
-    in
-    let rc = open_in "day_one_small" in
+    let rc = open_in "day_one_input" in
     let lines = read_file rc in
-    let rec f x =
-        let y = (x / 3) - 2 in
-        if y <= 0 then 0 else y + f y
-    in
-    let sum = List.fold_left (fun acc line -> acc + f (int_of_string line)) 0 lines in
+    let handle line = solve (int_of_string line) in
+    let solved_lines = List.map handle lines in
+    let sum = List.fold_left (+) 0 solved_lines in
     print_int sum;
     print_newline ();
     close_in rc;;
